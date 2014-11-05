dc2f461408b295bd9f0a23ef5430fa9fba5f91d6
Ben Chatelain <benchatelain@gmail.com>
2014-11-04 18:43:12 -0700

    Initial Commit

---
+++ CrashlyticsPodTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrashlyticsPodTest
+//
+//  Created by Ben Chatelain on 11/4/14.
+//  Copyright (c) 2014 phatblat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CrashlyticsPodTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrashlyticsPodTest
+//
+//  Created by Ben Chatelain on 11/4/14.
+//  Copyright (c) 2014 phatblat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
